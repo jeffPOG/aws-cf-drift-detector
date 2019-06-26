1563a7fe40694e1ad4db24e9af2b12a832c73ff7
Łukasz Pepliński <lukasz.peplinski@huuugegames.com>
2019-06-26 13:27:46 +0200

    changed deploy script - now bucket for stack code will be created if it's non-existent

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ while in `packages` folder:
 pip install -r requirements.txt -t .
 ```
 
-to install never versions of boto3 and boto3core needed to access drift detection api
+to install never versions of boto3 and boto3core needed to access drift detection api.
+
+Also check out `deploy.sh` script to know what's going on and to configure s3 bucket for stack code
+
+### Prerequisites needed to deploy
+* AWS CLI
+* AWS SAM
--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
+CODE_BUCKET_NAME="common-microservices-code"
+ADMIN_MAIL="lukpep@gmail.com"
+
 # validate template
 sam validate
+
+# check if s3 bucket exists
+if aws s3 ls "s3://$CODE_BUCKET_NAME" 2>&1 | grep -q 'NoSuchBucket'
+then
+    echo "Bucket $CODE_BUCKET_NAME does not exists - creating ..."
+    aws s3 mb s3://${CODE_BUCKET_NAME}
+fi
+
 
 # package and upload to S3
 # IMPORTANT! common-microservices-code bucket must be present on AWS S3
 sam package \
    --template-file template.yaml \
    --output-template-file generated-template.yaml \
-   --s3-bucket common-microservices-code
+   --s3-bucket ${CODE_BUCKET_NAME}
 
 # deploy stack
 sam deploy \
    --template-file generated-template.yaml \
    --stack-name DRIFT-DETECTOR-SERVICE \
    --capabilities CAPABILITY_IAM \
-   --parameter-overrides AdminMail=lukpep@gmail.com
+   --parameter-overrides AdminMail=${ADMIN_MAIL}
